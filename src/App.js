62847de585f43cc521aa8f285c0b6b6a84952b00
pengivig <pengivig@icloud.com>
2023-03-20 13:19:32 +0300

    r2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import LoginPage from './pages/LoginPage/LoginPage'
 import MainPage from './pages/MainPage/MainPage'
 import s from './App.module.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import NotFound from './pages/NotFound/NotFound.Page'
 
 function App() {
 	return (
 		<div className={s.app}>
 			<Routes>
-				<Route path='/hotels-simple-check/' element={<Navigate to='/hotels-simple-check/signIn' />} />
-				<Route path='/hotels-simple-check/signIn' element={<Navigate to='/hotels-simple-check/signIn' />} />
+				<Route path='/hotels-simple-check/*' element={<NotFound />} />
+				<Route path='/hotels-simple-check/' element={<Navigate to='/hotels-simple-check/signIn'/>} />
 				<Route path='/hotels-simple-check/signIn' element={<LoginPage />} />
 				<Route path='/hotels-simple-check/main' element={<MainPage />} />
 			</Routes>
---
+++ src/pages/NotFound/NotFound.Page.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+	return (
+		<div style={{ display: 'flex', justifyContent: 'center' }}>
+			<h1>Страница не найдена</h1>
+		</div>
+	)
+}
+
+export default NotFound
